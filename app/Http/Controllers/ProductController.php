1108509edf5193ad302bf6b16a0221f29e565344
Trương Minh Phung <minhphung485@gmail.com>
2025-06-15 13:46:17 +0700

    12

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -523,58 +523,6 @@ class ProductController extends Controller
         }
     }
     /**
-     * Display truongssg of t    esource
-     */
-    public function recommend(Request $request)
-    {
-        try {
-            $customerId = $request->input('customer_id');
-
-            $sub = DB::table('order_items')
-                ->join('orders', 'orders.id', '=', 'order_items.order_id')
-                ->where('orders.customer_id', $customerId)
-                ->where('orders.status', 'completed')
-                ->select(
-                    'order_items.id as item_id',
-                    'order_items.product_id',
-                    'order_items.stock_id',
-                    'order_items.attribute_first_id',
-                    'order_items.attribute_second_id',
-                    'order_items.sell_price',
-                    'order_items.quantity',
-                    'orders.transaction_date',
-                    DB::raw("ROW_NUMBER() OVER (
-            PARTITION BY order_items.product_id, order_items.attribute_first_id, order_items.attribute_second_id
-            ORDER BY orders.transaction_date DESC
-        ) as rn")
-                );
-
-            $recommend = DB::table(DB::raw("({$sub->toSql()}) as recent_items"))
-                ->mergeBindings($sub)
-                ->where('recent_items.rn', 1)
-                ->join('products', 'products.id', '=', 'recent_items.product_id')
-                ->leftJoin('attributes as a1', 'a1.id', '=', 'recent_items.attribute_first_id')
-                ->leftJoin('attributes as a2', 'a2.id', '=', 'recent_items.attribute_second_id')
-                ->leftJoin('stocks', 'stocks.id', '=', 'recent_items.stock_id')
-                ->select(
-                    'products.id as product_id',
-                    'products.name as product_name',
-                    'products.type as product_type',
-                    'stocks.name as stock_name',
-                    'a1.title as attribute_1',
-                    'a2.title as attribute_2',
-                    'recent_items.sell_price',
-                    'recent_items.quantity',
-                    'recent_items.transaction_date'
-                )
-                ->orderByDesc('recent_items.transaction_date')
-                ->get();
-            return $this->responseSuccess($recommend);
-        } catch (\Exception $e) {
-            return $this->responseError($e->getMessage(), 500);
-        }
-    }
-    /**
      * Show the form for creating a new resource.
      */
     public function create()
